797d715acbdd23f68c821710ccaf906449422caa
Ben <Benarnold60@gmail.com>
2026-03-01 20:41:38 +1100

    Restore default standard flashing catalog

--- src/data/standardFlashings.js
+++ src/data/standardFlashings.js
@@ -1,4 +1,58 @@
 export const STANDARD_FLASHING_DESIGNS = [
-
-
+  {
+    id: "std-side-apron-corro",
+    name: "Side Apron Corro",
+    folds: [
+      { length: 80, angle: 90 },
+      { length: 300, angle: 0 },
+      { length: 63, angle: 288 },
+    ],
+  },
+  {
+    id: "std-barge-rolltop",
+    name: "Barge Rolltop",
+    folds: [
+      { length: 35, angle: 90 },
+      { length: 180, angle: 0 },
+      { length: 20, angle: 270 },
+      { length: 18, angle: 0 },
+    ],
+  },
+  {
+    id: "std-ridge-capping",
+    name: "Ridge Capping",
+    folds: [
+      { length: 25, angle: 90 },
+      { length: 220, angle: 330 },
+      { length: 220, angle: 30 },
+      { length: 25, angle: 270 },
+    ],
+  },
+  {
+    id: "std-valley-tray",
+    name: "Valley Tray",
+    folds: [
+      { length: 20, angle: 90 },
+      { length: 130, angle: 330 },
+      { length: 130, angle: 30 },
+      { length: 20, angle: 270 },
+    ],
+  },
+  {
+    id: "std-apron-low",
+    name: "Apron Low",
+    folds: [
+      { length: 15, angle: 90 },
+      { length: 240, angle: 0 },
+      { length: 90, angle: 288 },
+    ],
+  },
+  {
+    id: "std-90-internal",
+    name: "Internal 90",
+    folds: [
+      { length: 120, angle: 0 },
+      { length: 120, angle: 270 },
+    ],
+  },
 ];
